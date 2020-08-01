5b823103336bf2348eeb40857c8cb0bdd5ce67fd
mariya_rangwala <mariya.rngwl@gmail.com>
2020-08-01 09:38:22 -0700

    Added Stateful and Stateless widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,66 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main()=> runApp(MaterialApp(
+void main()=> runApp(MyApp());
 
-  home: Scaffold(
-    body: Center(
-      child: Text('Hello World')
-    ),
-  ),
-));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello"),
+        ),
+        body: Counter(),
+        ),
+    );
+  }
+}
 
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
 
+class _CounterState extends State<Counter> {
+  int counter = 0;
 
+  void _increment(){
+    setState(() {
+     counter++;
+    });
+  }
 
+  void _decrement(){
+    setState(() {
+      counter--;
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _increment,
+          child: Text("+"),
 
+        ),
+        RaisedButton(
+          onPressed: _decrement,
+          child: Text("-"),
+        ),
+        Text("count= $counter"),
+      ],
 
+    );
+  }
+}
+class Count extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
